3e0ae0892b6b13cce2a8584eae42325fb9fc0ea2
Owen Kovalski <owen.kovalski@gmail.com>
2022-01-01 21:17:55 -0500

    Update footer.php

--- public/footer.php
+++ public/footer.php
@@ -39,6 +39,8 @@
 	</div>
 </footer>
 
+<?php printf( __( '&copy; %s. All Rights Reserved.', 'text-domain', '2021&ndash' )); echo date(‘Y’); ?>
+
 <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
 <script>
 AOS.init();
