985e78d2bb30cfac3abd81b04479677b9c0d5653
Owen Kovalski <owen.kovalski@gmail.com>
2022-01-20 22:09:03 -0500

    Update header.php

--- public/header.php
+++ public/header.php
@@ -27,7 +27,7 @@
 		    </button>
 		    <div class="collapse navbar-collapse" id="navbarResponsive">
 			    <ul class="navbar-nav ml-auto">
-				    <li class="nav-item active">
+				    <li class="nav-item">
 					    <a class="nav-link" href="index.php">Home</a>
 				    </li>
 				    <li class="nav-item">
