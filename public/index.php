d59c828c12cda4b332e6c5378f56300328a33481
Owen Kovalski <owen.kovalski@gmail.com>
2022-01-20 22:38:15 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -52,8 +52,7 @@
 	<div class="row jumbotron">
 		<div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-10">
 			<p class="lead">Website development has to start somewhere. Start with a domain name!</p>
-        <br>
-        <br>
+        <hr>
 		</div>
 		<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-2">
 			<a href="https://domains.google/"><button type="button" class="btn btn-outline-secondary
