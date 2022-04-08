ef3f679db311c3634cde3c7c234968688d0d9b77
Owen Kovalski <owen.kovalski@gmail.com>
2022-04-08 02:02:20 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -56,7 +56,7 @@
 		data-aos-duration="1000">
 	<div class="row jumbotron">
 		<div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-10">
-			<p class="lead">Website development has to start somewhere. Start with a domain name!</p>
+			<p style="text-align:center;font-size:20px;">Website development has to start somewhere. Start with a domain name!</p>
         <hr>
 		</div>
 		<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-2">
