619470776c2771067bc6cd86026f67a6f32c4d3b
Owen Kovalski <owen.kovalski@gmail.com>
2022-04-08 17:57:25 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -15,7 +15,7 @@
 		</h1> 
 		<br>
 		<br>
-		<p style="font-size: 20px;"> 
+		<p style="font-size: 20px; margin-left: 10%; margin-right: 10%;"> 
 			This website is a personal project of mine. I am currently utilizing it as a way to learn about website development, as well as, showcase skills and projects that I am working on. 
 		</p>
 	</div>
