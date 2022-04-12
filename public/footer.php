4c19a1dbff7542bec891ec1b00d89bc2350c1fe2
Owen Kovalski <owen.kovalski@gmail.com>
2022-04-11 21:35:00 -0400

    Update footer.php

--- public/footer.php
+++ public/footer.php
@@ -1,6 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Follow My IT Journey!</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">  
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+	<link href="/css/reset.css" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet">
+</head>
+
 <!--- Connect -->
 <hr>
 <div class="container-fluid padding">
