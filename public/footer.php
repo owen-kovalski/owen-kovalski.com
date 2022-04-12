cb3349b2166f7d71d44033465b659720fe9d8bc0
Owen Kovalski <owen.kovalski@gmail.com>
2022-04-11 22:14:29 -0400

    Update footer.php

--- public/footer.php
+++ public/footer.php
@@ -4,9 +4,9 @@
 	<div class="row text-center padding">
 		<div class="col-12">
 			<h2>Connect With Me</h2>
+			<hr>
 			<script src="https://tryhackme.com/badge/282152"></script>
 		</div>
-		<hr>
 		<div class="col-12 social padding">
 			<a href="https://www.facebook.com/OwenKovalskiwebsite/"><i class="fab fa-facebook"></i></a>
 			<a href="https://www.twitter.com/Owen_Kovalski/"><i class="fab fa-twitter"></i></a>
