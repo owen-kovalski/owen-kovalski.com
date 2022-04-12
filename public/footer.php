1d8a5d7c880982a3794bfb52f26d8358881e2bdb
Owen Kovalski <owen.kovalski@gmail.com>
2022-04-11 22:00:27 -0400

    Update footer.php

--- public/footer.php
+++ public/footer.php
@@ -32,7 +32,7 @@
 			width: 15%;
 			text-align: center;
 			padding: 0% 1% 1% 1%;
-			margin: 0% 40% 0% 40%;
+			margin: 0% 43% 0% 40%;
 			border: solid 5px green;
 			border-radius: 100px;
 			">
