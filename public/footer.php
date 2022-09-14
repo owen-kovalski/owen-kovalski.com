1f1528e183469b0716e5b7a0fea5b0b28d7f89e7
Owen Kovalski <owen.kovalski@gmail.com>
2022-09-14 15:02:46 -0400

    Update footer.php

--- public/footer.php
+++ public/footer.php
@@ -17,7 +17,7 @@
 		</div>
 	</div>
 </div>
-<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </body>
 
 <!--- Footer -->
