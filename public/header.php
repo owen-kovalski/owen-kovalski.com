ee824331bcb3873c3ad93d957a3170e47ad8c914
Owen Kovalski <owen.kovalski@gmail.com>
2022-09-14 14:57:09 -0400

    Update header.php

--- public/header.php
+++ public/header.php
@@ -14,7 +14,8 @@
 
 	<!-- Bootstrap CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	
 	<!-- Custom CSS -->
 	<link href="/css/reset.css" rel="stylesheet">
     <link href="/css/style.css" rel="stylesheet">
