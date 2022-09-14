dd173eba690e5e14d33a5c3bccf9704c40e5d07c
Owen Kovalski <owen.kovalski@gmail.com>
2022-09-14 19:19:44 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@
 <br>
 <section class="bg-dark text-light p-5 text-center text-sm-start">
 	<div class="container" 
-	     	data-aos="fade-up"
+	    data-aos="fade-up"
 		data-aos-offset="200"
 		data-aos-delay="50"
 		data-aos-duration="1000">
@@ -36,18 +36,20 @@
 <br>
 <br>
 <!--- Jumbotron -->
-<div class="container" data-aos="fade-up"
-		data-aos-offset="200"
-		data-aos-delay="50"
-		data-aos-duration="1000">
-	<div class="row jumbotron">
-		<div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-10">
-			<p style="text-align: center; font-size: 50px;">BLOG COMING SOON!</p>
-        <hr>
+<section class="bg-dark text-light p-5 text-center text-sm-start">
+	<div class="container" 
+			data-aos="fade-up"
+			data-aos-offset="200"
+			data-aos-delay="50"
+			data-aos-duration="1000">
+		<div class="row jumbotron">
+			<div class="d-sm-flex justify-content-between">
+				<h1 class="display-1">BLOG COMING SOON!</h1>
+        	<hr>
+			</div>
 		</div>
 	</div>
-</div>
-
+</section>
 <?php
     include_once 'footer.php';
 ?>
