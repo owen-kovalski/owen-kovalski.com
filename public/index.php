4cfedd7bc100f2ef00ce95944c757a4adcb05cd6
Owen Kovalski <owen.kovalski@gmail.com>
2022-09-14 11:09:30 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -17,7 +17,7 @@
 				<p class="lead my-4"> 
 					This website is a personal project of mine. I am currently utilizing it as a way to learn about website development, as well as, showcase skills and projects that I am working on. 
 				</p>
-				<button class="btn btn-outline-primary btn-lg">Get your Domain Name</button>
+				<a href="https://domains.google/"><button class="btn btn-outline-secondary btn-sm" style="text-color: white;">Get your Domain Name</button>
 			</div>
 			<img class="img-fluid w-50 d-none d-sm-block" src="img/background2.jpg" alt="" />	
 		</div>
@@ -37,12 +37,8 @@
 		data-aos-duration="1000">
 	<div class="row jumbotron">
 		<div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-10">
-			<p style="text-align: center; font-size: 50px;">Website development has to start somewhere. Start with a domain name!</p>
+			<p style="text-align: center; font-size: 50px;">BLOG COMING SOON!</p>
         <hr>
-		</div>
-		<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-2">
-			<a href="https://domains.google/"><button type="button" class="btn btn-outline-secondary
-			btn-lg" style="text-align: center;">Get Your Domain Name</button></a>
 		</div>
 	</div>
 </div>
