500ab55a69836c55fddec14947448f57155e2afc
Owen Kovalski <owen.kovalski@gmail.com>
2022-09-14 19:49:03 -0400

    Update blog.php

--- public/blog.php
+++ public/blog.php
@@ -2,6 +2,20 @@
     include_once 'header.php';
 	include_once '/css/style.css';
 ?>
+<section class="bg-dark text-light p-5 text-center text-sm-start">
+	<div class="container" 
+			data-aos="fade-up"
+			data-aos-offset="200"
+			data-aos-delay="50"
+			data-aos-duration="1000">
+		<div class="row jumbotron">
+			<div class="d-sm-flex justify-content-between">
+				<h1 class="display-1">COMING SOON!</h1>
+        	<hr>
+			</div>
+		</div>
+	</div>
+</section>
 <?php
     include_once 'footer.php';
 ?>
