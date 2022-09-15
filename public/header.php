c554833d7b4d55a1cfdf9e117ab496ac95bbc80c
Owen Kovalski <owen.kovalski@gmail.com>
2022-09-14 20:14:33 -0400

    Update header.php

--- public/header.php
+++ public/header.php
@@ -15,6 +15,9 @@
 	<!-- Bootstrap CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 	
+	<!-- Bootstrap Icon CSS -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
 	<!-- Custom CSS -->
 	<link href="/css/reset.css" rel="stylesheet">
     <link href="/css/style.css" rel="stylesheet">
