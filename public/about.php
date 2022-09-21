3ce7cd71625d1fb6ae1abd9bdd35beb5e750b829
Owen Kovalski <owen.kovalski@gmail.com>
2022-09-21 18:18:33 -0400

    Update about.php

--- public/about.php
+++ public/about.php
@@ -1,7 +1,17 @@
 <?php
     include_once 'header.php';
 ?>
-
+<section class="bg-dark text-light p-5 text-center text-sm-start">
+	<div class="container" 
+			data-aos="fade-up"
+			data-aos-offset="200"
+			data-aos-delay="50"
+			data-aos-duration="1000">
+            <div>
+                <h1>Who I am</h1>
+            </div>
+    </div>
+</section>
 <?php
     include_once 'footer.php';
 ?>
