0a3746a47945920b53198d16b74ad1e0729a30b9
Owen Kovalski <owen.kovalski@gmail.com>
2022-09-21 18:50:44 -0400

    Update about.php

--- public/about.php
+++ public/about.php
@@ -1,7 +1,7 @@
 <?php
     include_once 'header.php';
 ?>
-<section class="bg-dark text-light text-center">
+<section class="bg-dark text-light p-5 text-center">
 	<div class="container" 
 			data-aos="fade-up"
 			data-aos-offset="200"
@@ -18,7 +18,7 @@
 		data-aos-offset="200"
 		data-aos-delay="50"
 		data-aos-duration="1000">
-        <div class="mx-auto">
+        <div class="text-center p-5">
             <p class="lead">My name is <u>Owen Kovalski</u> and I am a 28 year old developer/cybersecurity researcher.</p>
         </div>
     </div>
