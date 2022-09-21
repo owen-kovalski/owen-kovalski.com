812c7dcc651d2012085a45a0f4e4deb7d8f842d9
Owen Kovalski <owen.kovalski@gmail.com>
2022-09-21 18:36:05 -0400

    Update about.php

--- public/about.php
+++ public/about.php
@@ -2,13 +2,13 @@
     include_once 'header.php';
 ?>
 <section class="bg-dark text-light p-5 text-center">
-	<div class="container" 
+	<div class="container padding" 
 			data-aos="fade-up"
 			data-aos-offset="200"
 			data-aos-delay="50"
 			data-aos-duration="1000">
             <div>
-                <h1>Who I am</h1>
+                <h1 class="display-1 mx-auto" style="width: 30%;">Who I am</h1>
             </div>
     </div>
 </section>
