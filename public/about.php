16eeed4046bbd9a787c053a7e8361866063b1a3d
Owen Kovalski <owen.kovalski@gmail.com>
2022-09-21 20:37:00 -0400

    Update about.php

--- public/about.php
+++ public/about.php
@@ -20,7 +20,9 @@
 		data-aos-duration="1000">
         <div class="text-center p-5">
             <p class="lead"><strong><u>Owen Kovalski</u></strong></p>
-            <p class="">A 28-year-old developer/cybersecurity researcher.</p>
+            <p>A 28-year-old developer and cybersecurity researcher.</p>
+            <p>Taking on various projects, bringing small businesses or individuals to the modern web.</p>
+            <p>By providing my services to clients, I allow them the ability to revitalize their brand for generations old and new!</p>
         </div>
     </div>
 </section>
