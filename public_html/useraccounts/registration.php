6f6e3b5f52a83dd29b82c88987173a3a5663271f
Owen Kovalski <owen.kovalski@gmail.com>
2022-12-20 14:35:04 -0500

    Update registration.php

--- public_html/useraccounts/registration.php
+++ public_html/useraccounts/registration.php
@@ -1,1 +1,15 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+  <title>User Registration | PHP</title>
+</head>
+<body>
+  
+  <div>
+    <form action="registration.php" method="post">
+      <div class="container">
+        <h1>Registration</h1>
+  </div>
+  
+</body
+</html>
