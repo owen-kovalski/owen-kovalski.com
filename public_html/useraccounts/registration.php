360f11ecdb89831b66d9606407b3a41c1aa93c08
Owen Kovalski <owen.kovalski@gmail.com>
2022-12-20 14:54:34 -0500

    Update registration.php

--- public_html/useraccounts/registration.php
+++ public_html/useraccounts/registration.php
@@ -9,6 +9,9 @@
     <form action="registration.php" method="post">
       <div class="container">
         <h1>Registration</h1>
+        <p>Please ensure all information is correct before submitting.</p>
+        <label for="firstname"><b>First Name</b></label>
+        <input type="text" name="firstname">
   </div>
   
 </body
