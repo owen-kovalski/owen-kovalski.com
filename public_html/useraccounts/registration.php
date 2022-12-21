1136e79e1dcb6823e711d7489ecc2b45fc81d191
Owen Kovalski <owen.kovalski@gmail.com>
2022-12-21 21:27:29 +0000

    modified:   public_html/useraccounts/registration.php

--- public_html/useraccounts/registration.php
+++ public_html/useraccounts/registration.php
@@ -11,7 +11,19 @@
         <h1>Registration</h1>
         <p>Please ensure all information is correct before submitting.</p>
         <label for="firstname"><b>First Name</b></label>
-        <input type="text" name="firstname">
+        <input type="text" name="firstname" required>
+
+        <label for="lastname"><b>Last Name</b></label>
+        <input type="text" name="lastname" required>
+
+        <label for="email"><b>Email Address</b></label>
+        <input type="email" name="email" required>
+
+        <label for="phonenumber"><b>Phone Number</b></label>
+        <input type="text" name="phonenumber" required>
+
+        <label for="password"><b>Password</b></label>
+        <input type="password" name="password" required>
   </div>
   
 </body
