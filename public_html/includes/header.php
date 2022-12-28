caab06c6f4cf432f3c0133159b293b81ea299e8b
Owen Kovalski <owen.kovalski@gmail.com>
2022-12-28 20:39:38 +0000

    Config for registration

--- public_html/includes/header.php
+++ public_html/includes/header.php
@@ -1,6 +1,3 @@
-<?php
-	include_once 'config.php';
-?>
 <!DOCTYPE html>
 <html lang="en">
 
--- public_html/registration.php
+++ public_html/registration.php
@@ -37,6 +37,8 @@
     </form>
   </div>
 </section>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
 <script type="text/javascript">
 	$(function(){
 		$('#register').click(function(e){
