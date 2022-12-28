857b3269cac1d673277529867cf92742e1dde90e
Owen Kovalski <owen.kovalski@gmail.com>
2022-12-28 15:01:59 +0000

    Changes to be committed: 	modified:   public_html/includes/header.php 	modified:   public_html/index.php

--- public_html/includes/header.php
+++ public_html/includes/header.php
@@ -23,29 +23,3 @@
 	<link href="/css/reset.css" rel="stylesheet">
     <link href="/css/style.css" rel="stylesheet">
 </head>
-
-<body>
-	<!-- Seperate Popper and Bootstrap JS -->
-	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-	    <div class="container-fluid">
-		    <a class="navbar-brand" href="index.php">
-				<img src="img/logo1.png" alt="" width="150" height="50">
-			</a>
-		    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-			    <span class="navbar-toggler-icon"></span>
-		    </button>
-		    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-			    <div class="navbar-nav">
-				    <a class="nav-link" href="index.php">Home</a>
-				    <a class="nav-link" href="blog.php">Blog</a>
-				    <a class="nav-link" href="about.php">About</a>
-				    <a class="nav-link" href="projects.php">Projects</a>
-				    <a class="nav-link" href="team.php">Team</a>
-				    <a class="nav-link" href="#">Login/Register</a>
-			    </div>
-		    </div>
-	    </div>
-    </nav>
