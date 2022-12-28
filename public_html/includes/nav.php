a5440b96b870e39a7e02d446fedbff60bb5486f2
Owen Kovalski <owen.kovalski@gmail.com>
2022-12-28 21:09:26 +0000

    Beautify Nav-bar

--- public_html/includes/nav.php
+++ public_html/includes/nav.php
@@ -9,7 +9,7 @@
 		    	<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 			    	<span class="navbar-toggler-icon"></span>
 		    	</button>
-		    	<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+		    	<div class="collapse navbar-collapse justify-content-right" id="navbarNavAltMarkup">
 			    	<div class="navbar-nav">
 				    	<a class="nav-link" href="index.php">Home</a>
 				    	<a class="nav-link" href="blog.php">Blog</a>
