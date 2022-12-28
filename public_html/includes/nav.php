766a4222db75a0eae54a83b42e810641d1066838
Owen Kovalski <owen.kovalski@gmail.com>
2022-12-28 21:02:03 +0000

    Login/Register Navbar

--- public_html/includes/nav.php
+++ public_html/includes/nav.php
@@ -17,12 +17,11 @@
 				    	<a class="nav-link" href="projects.php">Projects</a>
 				    	<a class="nav-link" href="team.php">Team</a>
 						<li class="nav-item dropdown">
-          					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+          					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Login/Register</a>
           				<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            				<li><a class="dropdown-item" href="#">Action</a></li>
-            				<li><a class="dropdown-item" href="#">Another action</a></li>
+            				<li><button class="btn btn-outline-success me-2" type="button" href="#">Login</button></li>
             				<li><hr class="dropdown-divider"></li>
-            				<li><a class="dropdown-item" href="#">Something else here</a></li>
+            				<li><button class="btn btn-sm btn-outline-secondary" type="button" href="registration.php">Register</button></li>
           				</ul>
 			    	</div>
 		    	</div>
