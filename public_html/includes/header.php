ac1f62595f20b7755ff180015c34d4163fdba851
Owen Kovalski <owen.kovalski@gmail.com>
2022-12-28 19:53:11 +0000

    Config fix 9

--- public_html/includes/header.php
+++ public_html/includes/header.php
@@ -1,6 +1,3 @@
-<?php
-  require_once('config.php');
-?>
 <!DOCTYPE html>
 <html lang="en">
 
--- public_html/registration.php
+++ public_html/registration.php
@@ -1,5 +1,6 @@
 <?php
 	include_once 'includes/header.php';
+  require_once('useraccounts/config.php');
 ?>
 <body>
 <?php
