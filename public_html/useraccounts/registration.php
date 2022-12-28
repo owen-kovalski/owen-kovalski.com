3634e9f8766d2c373654940fd0c56dba68f0c598
Owen Kovalski <owen.kovalski@gmail.com>
2022-12-28 16:06:08 +0000

    Changes to be committed: 	new file:   public_html/useraccounts/index.php 	modified:   public_html/useraccounts/registration.php

--- public_html/useraccounts/registration.php
+++ public_html/useraccounts/registration.php
@@ -18,24 +18,30 @@
   <div>
     <form action="registration.php" method="post">
       <div class="container">
-        <h1>Registration</h1>
-        <p>Please ensure all information is correct before submitting.</p>
-        <label for="firstname"><b>First Name</b></label>
-        <input type="text" name="firstname" required>
+        <div class="row">
+          <div class="col-sm-3">
+            <h1>Registration</h1>
+            <p>Please ensure all information is correct before submitting.</p>
+            <label for="firstname"><b>First Name</b></label>
+            <input class="form-control" type="text" name="firstname" required>
 
-        <label for="lastname"><b>Last Name</b></label>
-        <input type="text" name="lastname" required>
+            <label for="lastname"><b>Last Name</b></label>
+            <input class="form-control" type="text" name="lastname" required>
 
-        <label for="email"><b>Email Address</b></label>
-        <input type="email" name="email" required>
+            <label for="email"><b>Email Address</b></label>
+            <input class="form-control" type="email" name="email" required>
 
-        <label for="phonenumber"><b>Phone Number</b></label>
-        <input type="text" name="phonenumber" required>
+            <label for="phonenumber"><b>Phone Number</b></label>
+            <input class="form-control" type="text" name="phonenumber" required>
 
-        <label for="password"><b>Password</b></label>
-        <input type="password" name="password" required>
+            <label for="password"><b>Password</b></label>
+            <input class="form-control" type="password" name="password" required>
 
-        <input type="submit" name="create" value="Sign Up">
+            <input class="form-control" type="submit" name="create" value="Sign Up">
+          </div>
+        </div>
+      </div>
+    </form>
   </div>
 </body> 
 <?php
