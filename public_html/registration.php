9bb177088d936105d6cc294afdbcc5c04b999580
Owen Kovalski <owen.kovalski@gmail.com>
2022-12-28 20:04:09 +0000

    Possible config fix 12

--- public_html/registration.php
+++ public_html/registration.php
@@ -1,5 +1,6 @@
 <?php
 	include_once 'includes/header.php';
+  include_once 'useraccounts/config.php';
 ?>
 <body>
 <?php
--- public_html/useraccounts/config.php
+++ public_html/useraccounts/config.php
@@ -6,5 +6,3 @@ $db_name = "useraccounts";
 
 $db = new PDO('mysql:host=localhost;dbname=' . $db_name . ';charset=utf8', $db_user, $db_pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-?>
