59ad00df5d53e8a39be5801e0c70c5c6ad1f0ac7
Owen Kovalski <owen.kovalski@gmail.com>
2022-12-28 20:07:19 +0000

    Possible config fix 13

--- public_html/includes/header.php
+++ public_html/includes/header.php
@@ -1,3 +1,6 @@
+<?php
+	include_once 'config.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
--- public_html/registration.php
+++ public_html/registration.php
@@ -1,6 +1,5 @@
 <?php
 	include_once 'includes/header.php';
-  include_once 'useraccounts/config.php';
 ?>
 <body>
 <?php
