88680aa1659aea9c936191db60516f32c8986cef
Owen Kovalski <owen.kovalski@gmail.com>
2022-12-29 15:01:22 +0000

    Changes to be committed: 	modified:   public_html/includes/nav.php

--- public_html/includes/nav.php
+++ public_html/includes/nav.php
@@ -19,7 +19,7 @@
 						<li class="nav-item dropdown ">
           					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Login/Register</a>
           				<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            				<li><a class="dropdown-item mx-auto" href="login.php">Login</a></li>
+            				<li><a class="dropdown-item d-flex justify-content-center" href="login.php">Login</a></li>
             				<li><hr class="dropdown-divider"></li>
             				<li><a class="dropdown-item d-flex justify-content-center" href="registration.php">Register</a></li>
           				</ul>
