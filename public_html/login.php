b29dd4a21ea2a59c1c1e9c132a9f879b01ef5138
Owen Kovalski <owen.kovalski@gmail.com>
2022-12-29 18:39:47 +0000

    Login Form

--- public_html/login.php
+++ public_html/login.php
@@ -16,6 +16,39 @@
             <img src="userlogin/img/logo2.png" class="brand_logo" alt="Login Logo">
           </div>
         </div>
+        <div class="d-flex justify-content-center form_container">
+          <form>
+            <div class="input-group mb-3">
+              <div class="input-group-append">
+                <span class="input-group-text"><i class="fas fa-user"></i></span>
+              </div>
+              <input type="text" name="username" id="username" class="form-control input_user" required>
+            </div>
+            <div class="input-group mb-2">
+              <div class="input-group-append">
+                <span class="input-group-text"><i class="fas fa-user"></i></span>
+              </div>
+              <input type="text" name="password" id="password" class="form-control input_pass" required>
+            </div>
+            <div class="form-group">
+              <div class="custom-control custom-checkbox">
+                <input type="checkbox" name="rememberme" class="custom-control-input" id="customControlInline">
+                <label class="custom-control-label" for="customControlInline">Remember Me</label>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div class="d-flex justify-content-center mt-3 login-container">
+          <button type="button" name="button" id="login" class="btn login_btn">Login</button>
+        </div>
+        <div class="mt-4">
+          <div class="d-flex justify-content-center links">
+            Don't have an account? <a href="registration.php" class="ml-2">Sign Up</a>
+          </div>
+          <div class="d-flex justify-content-center">
+            <a href="">Forgot your password?</a>
+          </div>
+        </div>
       </div>
   </div>
 </div>
