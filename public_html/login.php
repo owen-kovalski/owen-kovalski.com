f704b9278fac17c190842a2c12f338220b9ebd4b
Owen Kovalski <owen.kovalski@gmail.com>
2022-12-29 15:38:20 +0000

    Changes to be committed: 	deleted:    public_html/img/background1_1.jpg 	deleted:    public_html/img/background2_1.jpg 	new file:   public_html/img/logo2.png 	modified:   public_html/login.php 	new file:   public_html/userlogin/css/styles.css

--- public_html/login.php
+++ public_html/login.php
@@ -2,10 +2,23 @@
 	include_once 'includes/header.php';
     require_once('userlogin/config.php');
 ?>
+<link rel="stylesheet" type="text/css" href="userlogin/css/style.css">
 <body>
 <?php
   include_once 'includes/nav.php';
 ?>
+
+<div class="dcontainer h-100">
+  <div class="d-flex justify-content-center h-100">
+      <div class="user_card">
+        <div class="d-flex justify-content-center">
+          <div class="brand_logo_container">
+            <img src="img/logo2.png" class="brand_logo" alt="Login Logo">
+          </div>
+        </div>
+      </div>
+  </div>
+</div>
 </body>
 <?php
   include_once 'includes/footer.php';
