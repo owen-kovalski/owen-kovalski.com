b4694398e90df1c1f0957d8a010c55930beed12d
Owen Kovalski <owen.kovalski@gmail.com>
2022-12-29 21:21:17 +0000

    Process Message updated

--- public_html/useraccounts/process.php
+++ public_html/useraccounts/process.php
@@ -15,9 +15,9 @@ if(isset($_POST)){
 		$stmtinsert = $db->prepare($sql);
 		$result = $stmtinsert->execute([$firstname, $lastname, $email, $phonenumber, $password]);
 		if($result){
-			echo 'Successfully saved.';
+			echo 'Thank you for signing up!.';
 		}else{
-			echo 'There were erros while saving the data.';
+			echo 'There was an error while registering.';
 		}
 }else{
 	echo 'No data';
