cd333800bf29e52f51040b8782131bd7ad86bfda
Owen Kovalski <owen.kovalski@gmail.com>
2022-12-29 14:36:34 +0000

    Changes to be committed: 	modified:   public_html/registration.php

--- public_html/registration.php
+++ public_html/registration.php
@@ -10,8 +10,8 @@
   <div>
     <form action="registration.php" method="post">
       <div class="container">
-        <div class="row">
-          <div class="col-sm-3 align-self-center">
+        <div class="row align-self-center">
+          <div class="col-sm-3">
             <h1>Sign Up</h1>
             <p>Please ensure all information is correct before submitting.</p>
             <hr class="mb-3">
