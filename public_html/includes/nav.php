c9574e4aba8276cdd6a205ae715d8fd62a917570
Owen Kovalski <owen.kovalski@gmail.com>
2022-12-29 14:57:13 +0000

    Changes to be committed: 	modified:   public_html/includes/nav.php

--- public_html/includes/nav.php
+++ public_html/includes/nav.php
@@ -6,10 +6,10 @@
 		    	<a class="navbar-brand" href="index.php">
 					<img src="img/logo1.png" alt="" width="150" height="50">
 				</a>
-		    	<button class="navbar-toggler mx-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+		    	<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 			    	<span class="navbar-toggler-icon"></span>
 		    	</button>
-		    	<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+		    	<div class="collapse navbar-collapse mx-auto" id="navbarNavAltMarkup">
 			    	<div class="navbar-nav mx-auto">
 				    	<a class="nav-link" href="index.php">Home</a>
 				    	<a class="nav-link" href="blog.php">Blog</a>
