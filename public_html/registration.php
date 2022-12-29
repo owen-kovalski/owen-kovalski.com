017690629470ef039df24aa782698aa25f4ba474
Owen Kovalski <owen.kovalski@gmail.com>
2022-12-29 14:21:33 +0000

    Center Registration form Attampt_1

--- public_html/registration.php
+++ public_html/registration.php
@@ -11,7 +11,7 @@
     <form action="registration.php" method="post">
       <div class="container">
         <div class="row">
-          <div class="col-sm-3">
+          <div class="col-sm-3 justify-content-center">
             <h1>Sign Up</h1>
             <p>Please ensure all information is correct before submitting.</p>
             <hr class="mb-3">
