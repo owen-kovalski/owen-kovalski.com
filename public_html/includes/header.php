b72f99fafba7fcf260250bc4ef29aaa3ffab1791
Owen Kovalski <owen.kovalski@gmail.com>
2022-12-29 18:55:54 +0000

    Icons

--- public_html/includes/header.php
+++ public_html/includes/header.php
@@ -23,6 +23,9 @@
 	<link href="css/reset.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
 
+	<!-- Icons -->
+	<link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-solid-rounded/css/uicons-solid-rounded.css'>
+
 	<!-- Seperate Popper and Bootstrap JS -->
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
