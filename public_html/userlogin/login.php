625d2f62e52bc2be252767dc40cb5e2a3b1a8345
Owen Kovalski <owen.kovalski@gmail.com>
2022-12-29 14:16:31 +0000

    Create Login/Logout files

---
+++ public_html/userlogin/login.php
@@ -0,0 +1,12 @@
+<?php
+	include_once 'includes/header.php';
+    require_once('userlogin/config.php');
+?>
+<body>
+<?php
+  include_once 'includes/nav.php';
+?>
+</body>
+<?php
+  include_once 'includes/footer.php';
+?>
