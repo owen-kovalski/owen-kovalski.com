390529764874d349b9acea1ca655f5af63dc1a81
Owen Kovalski <owen.kovalski@gmail.com>
2023-03-21 18:36:25 +0000

    v

--- public_html/registration.php
+++ public_html/registration.php
@@ -1,79 +1,77 @@
 <?php
-	include_once 'includes/header.php';
-  require_once('useraccounts/config.php');
+include_once 'includes/header.php';
+require_once 'useraccounts/config.php';
+require_once 'vendor/autoload.php';
 
-  $errors = array();
-  error_reporting(E_ALL);
-  ini_set('display_errors', 1);
+use ParagonIE\PasswordLib\PasswordLib;
 
-  if (isset($_POST['create'])) {
+$errors = array();
 
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $email = $_POST['email'];
-    $phonenumber = $_POST['phonenumber'];
-    $password = $_POST['password'];
+if (isset($_POST['create'])) {
+  $firstname = $_POST['firstname'];
+  $lastname = $_POST['lastname'];
+  $email = $_POST['email'];
+  $phonenumber = $_POST['phonenumber'];
+  $password = $_POST['password'];
 
-    // Validate firstname
-    if (empty($firstname)) {
-      $errors[] = "First name is required";
-    } else if (!preg_match("/^[a-zA-Z-' ]*$/", $firstname)) {
-      $errors[] = "Invalid first name format";
-    }
-
-    // Validate lastname
-    if (empty($lastname)) {
-      $errors[] = "Last name is required";
-    } else if (!preg_match("/^[a-zA-Z-' ]*$/", $lastname)) {
-      $errors[] = "Invalid last name format";
-    }
-
-    // Validate email
-    if (empty($email)) {
-      $errors[] = "Email is required";
-    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $errors[] = "Invalid email format";
-    }
-
-    // Validate phonenumber
-    if (!empty($phonenumber) && !preg_match("/^[0-9]{10}$/", $phonenumber)) {
-      $errors[] = "Invalid phone number format";
-    }
-
-    // Validate password
-    if (empty($password)) {
-      $errors[] = "Password is required";
-    } else if (strlen($password) < 8) {
-      $errors[] = "Password must be at least 8 characters long";
-    }
-
-    if (empty($errors)) {
-
-      // Add user to database
-      $password = password_hash($password, PASSWORD_DEFAULT); 
-      // Encrypt password before storing in database (security purpose)
-
-      $sql = "INSERT INTO users (firstname, lastname, email, phonenumber, password) VALUES (?, ?, ?, ?, ?)";
-      $stmt = $db->prepare($sql);
-      $result = $stmt->execute([$firstname, $lastname, $email, $phonenumber, $password]);
-
-      if ($result) {
-        echo "<script>
-                Swal.fire({
-                  'title': 'Successful',
-                  'text': 'User registered successfully',
-                  'type': 'success'
-                });
-              </script>";
-      } else {
-        $errors[] = "Failed to add user to database";
-      }
-
-    }
-
+  // Validate firstname
+  if (empty($firstname)) {
+    $errors[] = "First name is required";
+  } else if (!preg_match("/^[a-zA-Z-' ]*$/", $firstname)) {
+    $errors[] = "Invalid first name format";
   }
 
+  // Validate lastname
+  if (empty($lastname)) {
+    $errors[] = "Last name is required";
+  } else if (!preg_match("/^[a-zA-Z-' ]*$/", $lastname)) {
+    $errors[] = "Invalid last name format";
+  }
+
+  // Validate email
+  if (empty($email)) {
+    $errors[] = "Email is required";
+  } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $errors[] = "Invalid email format";
+  }
+
+  // Validate phonenumber
+  if (!empty($phonenumber) && !preg_match("/^[0-9]{10}$/", $phonenumber)) {
+    $errors[] = "Invalid phone number format";
+  }
+
+  // Validate password
+  if (empty($password)) {
+    $errors[] = "Password is required";
+  } else if (strlen($password) < 8) {
+    $errors[] = "Password must be at least 8 characters long";
+  }
+
+  if (empty($errors)) {
+    $passwordlib = new PasswordLib();
+    $password_hash = $passwordlib->createPasswordHash($password, PASSWORD_ARGON2ID);
+
+    // Add user to database
+    $sql = "INSERT INTO users (firstname, lastname, email, phonenumber, password) VALUES (?, ?, ?, ?, ?)";
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param('sssss', $firstname, $lastname, $email, $phonenumber, $password_hash);
+    $result = $stmt->execute();
+
+    if ($result) {
+      echo "<script>
+              Swal.fire({
+                'title': 'Successful',
+                'text': 'User registered successfully',
+                'type': 'success'
+              });
+            </script>";
+    } else {
+      $errors[] = "Failed to add user to database";
+    }
+  }
+}
 ?>
+
 <body>
 <?php
   include_once 'includes/nav.php';
@@ -93,47 +91,26 @@
             <?php if (in_array("Invalid first name format", $errors)) echo "<p class='text-danger'>Invalid first name format</p>"; ?>
 
             <label for="lastname"><b>Last Name</b><b style="color: red;"> *</b></label>
-			<input class="form-control" id="lastname" type="text" name="lastname" value="<?php echo isset($_POST['lastname']) ? htmlspecialchars($_POST['lastname']) : ''; ?>" required>
-			<?php if (isset($errors['lastname'])) { ?>
-    		<div class="alert alert-danger"><?php echo $errors['lastname']; ?></div>
-			<?php } ?>
+            <input class="form-control" id="lastname" type="text" name="lastname" value="<?php echo isset($_POST['lastname']) ? htmlspecialchars($_POST['lastname']) : ''; ?>" required>
+            <?php if (in_array("Last name is required", $errors)) echo "<p class='text-danger'>Last name is required</p>"; ?>
+            <?php if (in_array("Invalid last name format", $errors)) echo "<p class='text-danger'>Invalid last name format</p>"; ?>
 
+            <label for="email"><b>Email Address</b><b style="color: red;"> *</b></label>
+            <input class="form-control" id="email" type="email" name="email" value="<?php echo isset($_POST['email']) ? $_POST['email'] : ''; ?>" required>
+            <?php if (in_array("Email is required", $errors)) echo "<p class='text-danger'>Email is required</p>"; ?>
+            <?php if (in_array("Invalid email format", $errors)) echo "<p class='text-danger'>Invalid email format</p>"; ?>
 
-			<label for="email"><b>Email Address</b><b style="color: red;"> *</b></label>
-			<input class="form-control" id="email" type="email" name="email" required>
-			<?php if(isset($error['email'])) { ?>
-  			<div class="alert alert-danger"><?php echo $error['email']; ?></div>
-			<?php } ?>
+            <label for="phonenumber"><b>Phone Number</b><b class="text-muted"> (Optional)</b></label>
+            <input class="form-control" id="phonenumber" type="tel" name="phonenumber" value="<?php echo isset($_POST['phonenumber']) ? $_POST['phonenumber'] : ''; ?>">
+            <?php if (in_array("Invalid phone number format", $errors)) echo "<p class='text-danger'>Invalid phone number format</p>"; ?>
 
-			<label for="phonenumber"><b>Phone Number</b><b class="text-muted"> (Optional)</b></label>
-			<input class="form-control" id="phonenumber" type="text" name="phonenumber">
-			<?php
-			if (isset($_POST['phonenumber'])) {
-  				$phonenumber = $_POST['phonenumber'];
-  				if (!preg_match("/^[0-9]{10}$/", $phonenumber)) {
-    			$error['phonenumber'] = "Invalid phone number. Please enter a 10-digit phone number.";
-  					}
-			}
-			if (isset($error['phonenumber'])) {
-  				echo '<div class="alert alert-danger">' . $error['phonenumber'] . '</div>';
-			}
-			?>
+            <label for="password"><b>Password</b><b style="color: red;"> *</b></label>
+            <input class="form-control" id="password" type="password" name="password" required>
+            <?php if (in_array("Password is required", $errors)) echo "<p class='text-danger'>Password is required</p>"; ?>
+            <?php if (in_array("Password must be at least 8 characters long", $errors)) echo "<p class='text-danger'>Password must be at least 8 characters long</p>"; ?>
 
-			<label for="password"><b>Password</b><b style="color: red;"> *</b></label>
-			<input class="form-control" id="password" type="password" name="password" required>
-			<?php if(isset($error['password'])) { ?>
-    			<div class="alert alert-danger"><?php echo $error['password']; ?></div>
-			<?php } ?>
-			<hr class="mb-3">
-
-      //Submit button stores user information in database and redirects to login page if successful or displays error message if unsuccessful
-			<input class="btn btn-primary" type="submit" id="register" name="create" value="Sign Up" onclick="submitForm()">
-      <?php if ($result) {
-          echo "Data submitted successfully!";
-        } else {
-          echo "Error submitting data!";
-        } ?>
-        </div>
+            <button type="submit" class="btn btn-primary" name="create">Create Account</button>
+          </div>
         </div>
       </div>
     </form>
@@ -148,30 +125,25 @@
 			var valid = this.form.checkValidity();
 
 			if(valid){
-			var firstname 	= $('#firstname').val();
-			var lastname	  = $('#lastname').val();
-			var email 		  = $('#email').val();
-			var phonenumber = $('#phonenumber').val();
-			var password 	  = $('#password').val();
-				
-        e.preventDefault();	
+				e.preventDefault();
 				$.ajax({
 					type: 'POST',
 					url: 'useraccounts/process.php',
-					data: {firstname: firstname,lastname: lastname,email: email,phonenumber: phonenumber,password: password},
+					data: $('#registration-form').serialize(),
 					success: function(data){
-					Swal.fire({
-								'title': 'Successful',
-								'text': data,
-								'type': 'success'
-								})							
+						Swal.fire({
+							title: 'Successful',
+							text: data,
+							type: 'success'
+						});
+						document.getElementById("registration-form").reset();
 					},
 					error: function(data){
 						Swal.fire({
-								'title': 'Errors',
-								'text': 'There were errors while saving the data.',
-								'type': 'error'
-								})
+							title: 'Errors',
+							text: 'There were errors while saving the data.',
+							type: 'error'
+						});
 					}
 				});				
 			}else{				
@@ -180,12 +152,12 @@
 	});	
 </script>
 <script type="text/javascript">
-  function submitForm() {
-    var form = document.getElementById('registration-form');
-    form.submit();
-  }
+	function submitForm() {
+		var form = document.getElementById('registration-form');
+		form.submit();
+	}
 </script>
-</body> 
+</body>
 <?php
-  include_once 'includes/footer.php';
+	include_once 'includes/footer.php';
 ?>
