c143b59cf22a21cede4d0e994c4413aa154f9882
Owen Kovalski <owen.kovalski@gmail.com>
2023-03-22 18:59:25 +0000

    Edited to reflect the correct path to the autoload.php file.

--- public_html/registration.php
+++ public_html/registration.php
@@ -1,7 +1,7 @@
 <?php
 include_once 'includes/header.php';
 require_once 'useraccounts/config.php';
-require_once '../vendor/autoload.php';
+require_once('/home/okovalski/web/vendor/autoload.php');
 
 use ParagonIE\PasswordLib\PasswordLib;
 
